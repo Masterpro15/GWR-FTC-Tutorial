16f2cd72d71a02f14dc4ed059cc05a937f80f51b
Masterpro15 <evanckurian@gmail.com>
2025-07-26 11:58:43 -0500

    Making bonus tutorials

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Servos.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Servos.java
@@ -1,4 +1,49 @@
 package org.firstinspires.ftc.teamcode;
 
-public class Servos {
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+
+
+//This OpMode is a OpMode that moves a servo
+
+
+@TeleOp(name="Servos!", group="Linear OpMode")
+public class Servos extends LinearOpMode {
+
+    // Declare the variables that you need
+    //If you have more advanced java experience
+    private ElapsedTime runtime = new ElapsedTime();
+    private Servo exServo = null;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        exServo  = hardwareMap.get(Servo.class, "PUT THE NAME OF THE SERVO HERE");
+
+        waitForStart();
+        runtime.reset();
+
+        // This code runs in a loop while until the stop button is pressed
+        while (opModeIsActive()) {
+            //Servos are small motors that go to a certain position instead of spinning, they also have limited range so be sure to check your servo to see its range1
+            //Servos are programed with 0 being the minimum position and 1 being the maximum position.
+            if (gamepad1.a){ exServo.setPosition(0);} // When the a button is pressed, go to position zero
+            if (gamepad1.b){ exServo.setPosition(0.5);}
+            if (gamepad1.x){ exServo.setPosition(1);}
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
+        }
+    }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/simpleAuto.java
@@ -0,0 +1,71 @@
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+
+// This is a simple autonomous program BASED on a basic auto I used while competing as a rookie in FTC
+//Note that I use a bit more advanced Java concepts like methods in this program.
+@Autonomous(name=" Simple Auto", group="Robot")
+
+public class simpleAuto extends LinearOpMode {
+
+    //declare motors
+    private DcMotor leftDrive = null;
+    private DcMotor rightDrive = null;
+    private ElapsedTime     runtime = new ElapsedTime(); //we will use this timer to make the robot move for a certain amount of secodns
+
+
+    //Autonomous programs work like TeleOP programs but there is no while loop as autonmous only runs once
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //the "throws interrupted exception" just means that it expects a "wait(<seconds)" somewhere in the proggram
+        // Here we still initialize everything like normal
+        leftDrive = hardwareMap.get(DcMotor.class, "put name here");
+        rightDrive = hardwareMap.get(DcMotor.class, "put name from driver station here");
+
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        //now we wait for start again
+        waitForStart();
+        //Now we put our programs here, but first scroll down to see the method that I created
+        //here is where we run the method
+        moveForward(0.5, 2); //move forward at half speed for 2 seconds
+        sleep(1000); //wait for 1 second
+        moveForward(0.2, 1); //move forwards at a speed of 0.2 for 1 second
+        requestOpModeStop(); //we need to request the op mode to stop now because our autonomous program is done
+
+    }
+
+
+
+    // The method goes outside the runOpMode class
+
+    //this method will move the robot forward for a certain amount of time, putting the speed negative will make the robot go backwards
+    public void moveForward( double speed, double time){
+        leftDrive.setPower(speed); //we set the power for the motors
+        rightDrive.setPower(speed);
+        runtime.reset(); //reset the timer, and it starts counting again
+        while (opModeIsActive() && (runtime.seconds() < time)) {
+            //the robot will continue running for the time specified
+            telemetry.addLine("Moving Foward for " + time + " seconds." );
+            telemetry.update();
+        }
+        leftDrive.setPower(0); //stop the motors
+        rightDrive.setPower(0);
+    }
+
+
+
+
+}
